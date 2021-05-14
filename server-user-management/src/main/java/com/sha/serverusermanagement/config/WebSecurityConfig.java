1db3753122a631b3d7d333105c2799cca5432daa
Senol Atac <senol.atac.1@gmail.com>
2021-05-14 12:27:21 +0300

    initial

--- server-user-management/src/main/java/com/sha/serverusermanagement/config/WebSecurityConfig.java
+++ server-user-management/src/main/java/com/sha/serverusermanagement/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sha.serverusermanagement.config;
 
 import com.sha.serverusermanagement.jwt.JwtAuthorizationFilter;
 import com.sha.serverusermanagement.jwt.JwtTokenProvider;
+import com.sha.serverusermanagement.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private JwtTokenProvider tokenProvider;
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
